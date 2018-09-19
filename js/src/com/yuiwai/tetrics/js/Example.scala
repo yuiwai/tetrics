232202f1284c7963d49ee0c463ff1c20134fe8c5
Yuichiro Iwai <yuichiro.iwai@gmail.com>
2018-09-20 01:01:56 +0900

    ブロック消去時のエフェクト実装

--- js/src/com/yuiwai/tetrics/js/Example.scala
+++ js/src/com/yuiwai/tetrics/js/Example.scala
@@ -7,7 +7,6 @@ import org.scalajs.dom.html.Canvas
 import org.scalajs.dom.raw.{KeyboardEvent, MessageEvent}
 import org.scalajs.dom.{CanvasRenderingContext2D => Context2D}
 
-import scala.scalajs.js.Date
 import scala.scalajs.js.typedarray._
 
 object Example extends Subscriber with DefaultSettings {
@@ -20,7 +19,7 @@ object Example extends Subscriber with DefaultSettings {
   private implicit val ctx: Context2D =
     canvas.asInstanceOf[Canvas].getContext("2d").asInstanceOf[Context2D]
   private var game = new TenTen[KeyboardEvent, Context2D]
-    with JsController with JsView with AnimationComponent[KeyboardEvent, Context2D]
+    with JsController with JsView with JsCanvasAnimation
   private val serializer = new ByteEventSerializer {
     val setting: TetricsSetting = self.setting
   }
@@ -99,12 +98,11 @@ trait JsView extends LabeledFieldView[Context2D] {
     field.rows.zipWithIndex.foreach { case (row, y) =>
       (0 until row.width) foreach { x =>
         ctx.beginPath()
+        ctx.strokeStyle = "rgb(51, 51, 51)"
         if ((row.cols >> x & 1) == 0) {
-          ctx.fillStyle = "yellow"
-          ctx.strokeStyle = "grey"
+          ctx.fillStyle = "black"
         } else {
-          ctx.fillStyle = "red"
-          ctx.strokeStyle = "grey"
+          ctx.fillStyle = "darkgreen"
         }
         ctx.rect(x * tileWidth + offsetX, y * tileHeight + offsetY, tileWidth, tileHeight)
         ctx.fill()
@@ -115,10 +113,10 @@ trait JsView extends LabeledFieldView[Context2D] {
   def drawLabel(label: Label, offsetX: Int, offsetY: Int)
     (implicit ctx: Context2D): Unit = {
     ctx.beginPath()
-    ctx.fillStyle = "white"
+    ctx.fillStyle = "black"
     ctx.rect(offsetX, offsetY, tileWidth * fieldSize, labelHeight)
     ctx.fill()
-    ctx.fillStyle = "black"
+    ctx.fillStyle = "white"
     ctx.font = "12px Arial"
     ctx.textAlign = "left"
     ctx.textBaseline = "top"
@@ -152,12 +150,13 @@ trait AnimationComponent[E, C] extends TetricsGame[E, C] {
   override def beforeAction(action: TetricsAction): TetricsAction = {
     super.beforeAction(action)
     action match {
-      case n: NormalizeAction =>
-        tetrics.field(n.fieldType).filledRows match {
+      case dn: DropAndNormalizeAction =>
+        tetrics.act(dn.dropAction).field(dn.fieldType).filledRows match {
           case s: Seq[Int] if s.nonEmpty =>
-            block(n)
-            NoAction
-          case _ => action
+            animation = Some(BlockDeletionAnimation(dn.fieldType, s))
+            block(dn.normalizeAction)
+            dn.dropAction
+          case _ => dn
         }
       case _ => action
     }
@@ -165,15 +164,79 @@ trait AnimationComponent[E, C] extends TetricsGame[E, C] {
   private var animation: Option[Animation[C]] = None
   override def update(delta: Double)(implicit ctx: C, setting: TetricsSetting): Unit = {
     super.update(delta)
-    // FIXME 暫定対応
-    unblock()
-    // animation foreach()
+    animation match {
+      case Some(a) =>
+        animation = a.update(delta)
+        animation foreach draw
+      case None =>
+        drawAll(tetrics)
+        unblock()
+    }
   }
-  def draw()(implicit ctx: C): Unit = {
+  def draw(animation: Animation[C])(implicit ctx: C): Unit
+}
+trait JsCanvasAnimation extends AnimationComponent[KeyboardEvent, Context2D] {
+  override def draw(animation: Animation[Context2D])(implicit ctx: Context2D): Unit = {
+    animation match {
+      case b@BlockDeletionAnimation(fieldType, filledRows, _) =>
+        drawAll(tetrics)
+        ctx.beginPath()
+        ctx.fillStyle = s"rgba(255,255,255,${1 - b.rate})"
+        fieldType match {
+          case FieldLeft =>
+            filledRows.foreach { x =>
+              ctx.rect(
+                offset + tileWidth * (fieldSize - x - 1),
+                offset + (tileHeight * (fieldSize + 1)),
+                tileWidth,
+                tileHeight * fieldSize
+              )
+            }
+          case FieldRight =>
+            filledRows.foreach { x =>
+              ctx.rect(
+                offset + tileWidth * ((fieldSize * 2) + 2 + x),
+                offset + (tileHeight * (fieldSize + 1)),
+                tileWidth,
+                tileHeight * fieldSize
+              )
+            }
+          case FieldTop =>
+            filledRows.foreach { y =>
+              ctx.rect(
+                offset + tileWidth * (fieldSize + 1),
+                offset + (tileHeight * (fieldSize - y - 1)),
+                tileWidth * fieldSize,
+                tileHeight
+              )
+            }
+          case FieldBottom =>
+            filledRows.foreach { y =>
+              ctx.rect(
+                offset + tileWidth * (fieldSize + 1),
+                offset + (tileHeight * ((fieldSize * 2) + 2 + y)),
+                tileWidth * fieldSize,
+                tileHeight
+              )
+            }
+          case _ =>
+        }
+        ctx.fill()
+      case _ =>
+    }
   }
 }
-trait Animation[C]
-trait BlockingAnimation[C] extends Animation[C]
+trait Animation[C] {
+  def update(delta: Double): Option[Animation[C]]
+}
+case class BlockDeletionAnimation[C](fieldType: FieldType, filledRows: Seq[Int], now: Double = 0) extends Animation[C] {
+  val long = 400.0
+  def rate: Double = now / long
+  override def update(delta: Double): Option[Animation[C]] = now + delta match {
+    case t if t > long => None
+    case t => Some(copy(now = t))
+  }
+}
 
 trait JsMatchConnector {
 
